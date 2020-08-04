f51f9e85e177174fd00f71cbf0a0655d055f1848
Mallarapu, Ananth <ananth.mallarapu@cgi.com>
2020-08-04 17:57:10 +0530

    changed fucntionality to pass the simple tests

--- src/banana/Calculator.java
+++ src/banana/Calculator.java
@@ -1,11 +1,25 @@
 package banana;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Calculator {
 
 
 		
 		public int add (String numbers ) {
-			int result = 0  ; 
+			int result = 0 ;
+			if (numbers == "") {
+				return result ; 
+			}
+			
+			List<String> numberArrays = Arrays.asList(numbers.split("\\s*,\\s*"));
+			if (numberArrays.size() == 1)
+			{
+				return Integer.parseInt(numberArrays.get(0));
+			}
+			result = Integer.parseInt(numberArrays.get(0)) +  Integer.parseInt(numberArrays.get(1)) ;
+			
 			
 			return result ;
 		}
